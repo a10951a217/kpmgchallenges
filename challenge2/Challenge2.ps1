e15d96bc469a4ec5147fe3ba811f18653179ad40
a10951a217 <narasimhareddyc2@gmail.com>
2023-07-16 17:18:35 +0530

    first commit

---
+++ challenge2/Challenge2.ps1
@@ -0,0 +1,6 @@
+﻿$Token = Get-AzAccessToken
+$Headers = @{
+    'Authorization' = "Bearer $($Token.Token)"
+    'Content-Type' = 'application/json'
+}
+Invoke-RestMethod -Headers $Headers -Method Get -Uri "https://management.azure.com/subscriptions/7f735fc4-f2f3-41f3-bfce-4ecb4561d22d/resourcegroups/AKSdemo-rg/providers/Microsoft.Network/virtualNetworks/samplevnet?api-version=2015-06-15" | ConvertTo-Json | Out-File "C:\Users\naras\OneDrive\Desktop\challenges\output.txt"
---
+++ challenge3/challenge3.ps1
@@ -0,0 +1,20 @@
+﻿function Get-ValueFromNestedObject {
+    param (
+        [Parameter(Mandatory=$true)]
+        [object]$Object,
+        [Parameter(Mandatory=$true)]
+        [string]$Key
+    )
+    $keys = $Key.Split('/')
+    $value = $Object
+    foreach ($k in $keys) {
+        $value = $value.$k
+    }
+    return $value
+}
+
+
+$object = @{a=@{b=@{c='d'}}}
+$key = 'a/b/c'
+$value = Get-ValueFromNestedObject -Object $object -Key $key
+Write-Host "Value: $($value)"
